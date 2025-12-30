b9a7f0acf2ad8320dc6a6cda3b9c7a51e6f65742
Abhishek Tiwari <abhishektiwari78563@gmail.com>
2025-12-30 19:42:43 +0530

    Create pro2.c

---
+++ pro2.c
@@ -0,0 +1,87 @@
+//#include<stdio.h>
+
+//1.wap to find area of circle
+//void main(){
+//	float r,area;
+//	printf("Enter radius:");
+//	scanf("%f",&r);
+//	area=3.14*r*r;
+//	printf("Area of circle is %f",area);	
+//}
+
+//2.wap to find area of square
+//   void main(){
+//   	float side,area;
+//   	printf("Enter side:");
+//   	scanf("%f",&side);
+//   	area=side*side;
+//   	printf("Area of square is %f",area);
+//   }
+
+//3.wap to find area of rectangle;
+//   void main(){
+//   	
+//   	int length,bredth,area;
+//   	printf("Enter length:");
+//   	scanf("%d",&length);
+//   	printf("Enter bredth:");
+//   	scanf("%d",&bredth);
+//   	area=length*bredth;
+//   	printf("Area of reactangle is %d",area);
+//   	
+//   }
+
+//4.wap to find area of triangle
+//  void main(){
+//  	int b,h,area;
+//  	printf("Enter base:");
+//   	scanf("%d",&b);
+//   	printf("Enter height:");
+//   	scanf("%d",&h);
+//   	area=b*h/2;
+//   	printf("Area of triangle is %d",area);
+//  }
+
+//5.wap to find simple interest
+//  void main(){
+//  	float p,r,t,si;
+//  	printf("Enter p r t:");
+//  	scanf("%f%f%f",&p,&r,&t);
+//  	si=p*r*t/100;
+//  	printf("Simple interest is %f",si);
+//  }
+
+//6.wap to find perimeter of circle
+//  void main(){
+//  		float r,p;
+//  		printf("Enter radius:");
+//  		scanf("%f",&r);
+//  		p=2*3.14*r;
+//  		printf("Perimeter of circle is %f",p);
+//  }
+
+//7.wap to swap two variables using third variables
+//   void main(){
+//   	int a;//10
+//   	int b;//20
+//   	int c;
+//   	printf("Enter a and b:");
+//   	scanf("%d%d",&a,&b);
+//   	c=b;  //c=20
+//   	b=a;  //b=10
+//   	a=c;  //a=20;
+//   	printf("a=%d \n b=%d",a,b);
+//   }
+
+//8.wap to swap two variables without using third variables.
+
+//    void main(){
+//   	int a;//10     	
+//     	int b;//20
+//  	printf("Enter a and b:");
+//  	scanf("%d%d",&a,&b);
+//      a=a+b; //a=30
+//      b=a-b; //b=10
+//      a=a-b; //a=20
+//   	printf("a=%d \n b=%d",a,b);
+//   }
