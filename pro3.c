3e14198587baae9d5846d3fb5cd3f1370ab68b2a
Abhishek Tiwari <abhishektiwari78563@gmail.com>
2025-12-30 21:12:06 +0530

    Add commented code for various mathematical formulas

---
+++ pro3.c
@@ -0,0 +1,71 @@
+//#include<stdio.h>
+
+///1.WAP to find a the answer given formula (x+y)2.
+//void main(){
+//	float x,y,ans;
+//	printf("Enter x and y:");
+//	scanf("%f%f",&x,&y);
+//	ans=x*x+y*y+2*x*y;
+//	printf("The answer of (x+y)2 is %f",ans);
+//}
+
+//2.WAP to find a the answer given formula (x-y)2.
+//void main(){
+//	float x,y,ans;
+//	printf("Enter x and y:");
+//	scanf("%f%f",&x,&y);
+//	ans=x*x-y*y+2*x*y;
+//	printf("The answer of (x+y)2 is %f",ans);
+//}
+
+//3.WAP to find a the answer given formula (x+y)3.
+//void main(){
+//	float x,y,ans;
+//	printf("Enter x and y:");
+//	scanf("%f%f",&x,&y);
+//	ans=(x+y)*(x+y)*(x+y);
+//	printf("The answer of (x+y)3 is %f",ans);
+//}
+
+//4.WAP to find a the answer given formula (x-y)3.
+//void main(){
+//	float x,y,ans;
+//	printf("Enter x and y:");
+//	scanf("%f%f",&x,&y);
+//	ans = x*x*x - 3*x*x*y + 3*x*y*y - y*y*y;1
+//	printf("The answer of (x+y)3 is %f",ans);
+//}
+
+//5.WAP to find a the answer given formula (x+y+z)3.
+//void main(){
+//	int x,y,z,ans;
+//	printf("Enter x,y,z:");
+//	scanf("%d%d%d",&x,&y,&z);
+//	 ans = x*x*x + y*y*y + z*z*z
+//          + 3*(x+y)*(y+z)*(z+x);
+//	printf("The answer of (x+y+z)3 is %d",ans);
+//}
+
+//6.WAP to find a the answer given formula (x-y-z)3.
+//void main(){
+//	int x,y,z,ans;
+//	printf("Enter x,y,z:");
+//	scanf("%d%d%d",&x,&y,&z);
+//
+//    ans = (x - y - z) * (x - y - z) * (x - y - z);
+//	printf("The answer of (x-y-z)3 is %d",ans);
+//}
+
+//7.WAP to find a the answer given formula (x+y+z)2.
+//void main(){
+//    int x, y, z;
+//    int ans;
+//
+//    printf("Enter x y z: ");
+//    scanf("%d%d%d",&x,&y,&z);
+//
+//    ans = x*x + y*y + z*z + 2*(x*y + y*z + z*x);
+//
+//    printf("The answer of (x+y+z)^2 is %d", ans);
+//}
+
