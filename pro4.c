875d50585d75ccc7adfb447f7d8037136fd6568d
Abhishek Tiwari <abhishektiwari78563@gmail.com>
2025-12-30 22:05:02 +0530

    Create pro4.c

---
+++ pro4.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+
+//1.WAP to Find min. no. from given 2 no. using if else.
+//void main(){
+//	int a;
+//	int b;
+//	printf("Enter a and b:");
+//	scanf("%d%d",&a,&b);
+//	
+//	if(a>b){
+//		printf("a is greater");
+//	}else{
+//		printf("b is greater");
+//	}
+//}
+
+//2. WAP to Find if a given no. is neutral or not using ladder if else.
+//void main(){
+//	int num;
+//	printf("Enter num:");
+//	scanf("%d",&num);
+//	
+//	if(num>0){
+//	   printf("Given num is positive");	
+//	}else if(num<0){
+//		printf("Given number is negative");
+//	}else{
+//		printf("Given number is neutral");
+//	}
+//}
