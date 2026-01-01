0c2137740d351146fdec540b16cae5f5ec06beba
Abhishek Tiwari <abhishektiwari78563@gmail.com>
2026-01-01 17:57:29 +0530

    Create pro7.c

---
+++ pro7.c
@@ -0,0 +1,327 @@
+// #include<stdio.h>
+
+//1 wap to print 1 to n using for loop
+// void main(){ 
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//   for(i;i<=n;i++){
+//      printf("%d,",i);
+//   }
+// }
+
+
+//2 wap to print 1 to n using while loop 
+// void main(){   
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//   while(i<=n){
+//      printf("%d,",i);
+//       i++;
+//   }
+// }
+
+
+
+//3wap to print 1 to n using do..while loop
+// void main(){  
+//  int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//  do{
+//       printf("%d,",i);
+//       i++;
+//   }while(i<=n);
+// }
+
+//4 wap to print n to 1 using for loop
+// void main(){ 
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i;
+//  
+//   for(i=n;i>=1;i--){//n=10
+//      printf("%d,",i);
+//      
+//   }
+// }
+
+//5 wap to print n to 1 using while loop
+// void main(){   
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//   while(n>=i){
+//      printf("%d,",n);
+//       n--;
+//   }
+// }
+
+//6 wap to print n to 1 using do....while loop
+// void main(){  
+//  int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//  do{
+//       printf("%d,",n);
+//    n--;
+//   }while(n>=i);
+// }
+
+//7 wap to print odd no from 1 to n using for loop
+// void main(){ 
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i;
+//  
+//   for(i=n;i>=1;i--){
+//      if(i%2==1){
+//      	printf("%d,",i);
+//	  }
+//   }
+// }
+
+//7 wap to print odd no from 1 to n using for loop
+// void main(){   
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//   while(n>=i){
+//   	if(n%2==1){
+//   		printf("%d,",n);
+//	   }
+//	   n--;
+//   }
+// }
+
+
+//7 wap to print odd no from 1 to n using for loop
+//void main(){  
+//  int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//  do{
+//       if(n%2==1){
+//	   printf("%d,",n);
+//	   }
+//    n--;
+//   }while(n>=i);
+// }
+
+//8 wap to print even number from 1 to n using for loop
+// void main(){ 
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i;
+//  
+//   for(i=n;i>=1;i--){
+//      if(i%2==0){
+//      	printf("%d,",i);
+//	  }
+//   }
+// }
+
+//9 wap to print even number from 1 to n using while loop
+//void main(){   
+//   int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//   while(n>=i){
+//   	if(n%2==0){
+//   		printf("%d,",n);
+//	   }
+//	   n--;
+//   }
+// }
+
+//10 wap to print even number from 1 to n using do..while loop
+//void main(){  
+//  int n;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//  do{
+//       if(n%2==0){
+//	   printf("%d,",n);
+//	   }
+//    n--;
+//   }while(n>=i);
+// }
+
+//11 wap to find leap year from 2000 to 2050 using for loop
+//  void main(){ 
+//   int y;
+//   printf("Enter year:");
+//   scanf("%d",&y);
+//   int i=2000;
+//  
+//   for(i;i<=y;i++){
+//      if(i%4==0){
+//      	printf("%d,",i);
+//	  }
+//   }
+// }
+
+//12 wap to find leap year from 2000 to 2050 using while loop
+//void main(){   
+//   int y;
+//   printf("Enter year:");
+//   scanf("%d",&y);
+//   int i=2000;
+//  
+//   while(i<=y){
+//   	if(i%4==0){
+//   		printf("%d,",i);
+//	   }
+//	   i++;
+//   }
+// }
+
+//13 wap to find leap year from 2000 to 2050 using do..while loop
+//void main(){  
+//  int y;
+//   printf("Enter year:");
+//   scanf("%d",&y);
+//   int i=2000;
+//  
+//  do{
+//       if(i%4==0){
+//	   printf("%d,",i);
+//	   }
+//    i++;
+//   }while(i<=y);
+// }
+
+//14 wap to print sum of 1 to n using for loop
+// void main(){
+// 	int n,sum=0;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//   for(i;i<=n;i++){
+//   	sum=sum+i;
+//   }
+//   	printf("%d",sum);
+// }
+
+//15 wap to print sum of 1 to n using while loop
+//void main(){
+//   int n,sum=0;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//   while(i<=n){
+//   	sum=sum+i;
+//   	i++;
+//   }
+//    printf("%d",sum);  	
+//}
+
+//16 wap to print sum of 1 to n using do..while loop 
+//void main(){ 
+//  int n,sum=0;
+//   printf("Enter n:");
+//   scanf("%d",&n);
+//   int i=1;
+//  
+//  do{
+//    sum=sum+i;
+//    i++;
+//   }while(i<=n);
+//   printf("%d",sum);
+//}
+
+//17 wap to print factorial of n using for loop
+//void main(){
+//	int n,mul=1;
+//	printf("Enter n:");
+//	scanf("%d",&n);
+//	int i=1;
+//	for(i;i<=n;i++){
+//		mul=mul*i;
+//	}
+//		printf("%d",mul);
+//}
+
+//18 wap to print factorial of n using while loop
+//void main(){
+//	int n,mul=1;
+//	printf("Enter n:");
+//	scanf("%d",&n);
+//	int i=1;
+//	while(i<=n){
+//		mul=mul*i;
+//		i++;
+//	}
+//	printf("%d",mul);
+//}
+
+//19 wap to print factorial of n using do..while loop
+//void main(){
+//	int n,mul=1;
+//	printf("Enter n:");
+//	scanf("%d",&n);
+//	int i=1;
+//	do{
+//		mul=mul*i;
+//		i++;
+//	}while(i<=n);
+//	printf("%d",mul);
+//}
+
+//20 wap to print multiplication table of n using for loop
+//void main(){
+//	int n;
+//	printf("Enter n:");
+//	scanf("%d",&n);
+//	int i;
+//	for( i=1;i<=10;i++){
+//		printf("%d*%d=%d\n",n,i,n*i);
+//	}
+//}
+
+//21 wap to print multiplication table of n using while loop
+//void main(){
+//	int n;
+//	printf("Enter n:");
+//	scanf("%d",&n);
+//	int i=1;
+//	while(i<=10){
+//		printf("%d*%d=%d\n",n,i,n*i);
+//		i++;
+//	}
+//}
+
+//22 wap to print multiplication table of n using do..while loop
+//void main(){
+//	int n;
+//	printf("Enter n:");
+//	scanf("%d",&n);
+//	int i=1;
+//    do{
+//    	printf("%d*%d=%d\n",n,i,n*i);
+//    	i++;
+//	}while(i<=10);
+//}
+
