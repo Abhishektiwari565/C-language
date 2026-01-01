500b00eaeb8a2ea08271cd8542f980c27c1a2d9c
Abhishek Tiwari <abhishektiwari78563@gmail.com>
2026-01-01 20:53:04 +0530

    Create pro8.c

---
+++ pro8.c
@@ -0,0 +1,47 @@
+//#include<stdio.h>
+
+//1.wap to convert temperature from degree celsius to fahrenheit using the formula.
+//void main(){
+//	float c,f;
+//	printf("Enter temperature in celsius:");
+//	scanf("%f",&c);
+//	f=(c*9/5)+32;
+//	printf("Temperature in Fahrenheit = %.2f", f);
+//}
+
+//2.create a program that calculates the gross salary by adding percentages	of HRA, DA and TA to the base salary as chosen by user
+//void main() {
+//    float basic, hraP, daP, taP;
+//    float hra, da, ta, gross;
+//
+//    printf("Enter basic salary: ");
+//    scanf("%f", &basic);
+//
+//    printf("Enter HRA percentage: ");
+//    scanf("%f", &hraP);
+//
+//    printf("Enter DA percentage: ");
+//    scanf("%f", &daP);
+//
+//    printf("Enter TA percentage: ");
+//    scanf("%f", &taP);
+//
+//    hra = basic * hraP / 100;
+//    da  = basic * daP / 100;
+//    ta  = basic * taP / 100;
+//
+//    gross = basic + hra + da + ta;
+//
+//    printf("Gross Salary = %.2f", gross);
+//}
+
+//3 write a program to find third angle of triangle when two others angles are given.
+//void main(){
+//	int angle1,angle2,angle3;
+//	printf("Enter First Angles:");
+//	scanf("%d",&angle1);
+//	printf("Enter Second Angles:");
+//	scanf("%d",&angle2);
+//	angle3=180-(angle1+angle2);
+//	printf("Third angle=%d",angle3);
+//}
